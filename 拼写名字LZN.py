e4952b023c0e4e8d89412c7abebc3c5ad8138309
LiuZhaoning <31983056+LiuZhaoning@users.noreply.github.com>
2017-09-15 16:12:51 +0800

    Add files via upload

---
+++ 拼写名字LZN.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("L     ZZZZZZ N    N")
+print("L         Z  N   NN")
+print("L        Z   N  N N")
+print("L       Z    N N  N")
+print("L      Z     NN   N")
+print("LLLLL ZZZZZZ N    N")
